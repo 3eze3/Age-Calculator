99100becb6ab274de35b39e8c4f335ceedeeed2b
Ezequiel Cordova <ezekics33@gmail.com>
2023-06-25 20:43:07 -0500

    Add the test files

---
+++ unit-tests/dateComponent.test.js
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+import { dateComponent } from "../build/js/dateComponent";
+
+describe("dateComponent", () => {
+  it("Should be a class", () => expect(dateComponent).toBeTypeOf("function"));
+  it("The constructor should be ", () => expect());
+});
